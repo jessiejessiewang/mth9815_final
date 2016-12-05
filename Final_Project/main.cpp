8260d9877a3d6f0e09e9589ca3150b80940665b8
Jessie Wang <jessiewang0907@gmail.com>
2016-12-05 14:53:13 -0500

    Initial Commit

---
+++ Final_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final_Project
+//
+//  Created by Jessie Wang on 2016/12/5.
+//  Copyright © 2016年 Jessie Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
